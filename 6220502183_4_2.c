b73767072bdee3d99a53f02d6fb975e153e58431
saudrey <weerawutearth@gmail.com>
2021-02-25 16:02:50 +0700

    Master Chef B

---
+++ 6220502183_4_2.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main()
+{
+    int a,i,j;
+
+    scanf("%d",&a);
+    
+    int b[3][3];
+
+    for ( i = 0; i < a; i++)
+    {
+        for ( j = 0; j < 3; j++)
+        {
+            scanf("%d",b[i][j]);
+        }
+        
+    }
+    
+}
