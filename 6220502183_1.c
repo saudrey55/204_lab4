118baf49378d2268baf6dfbfdc74786501eb817d
saudrey <weerawutearth@gmail.com>
2021-02-25 15:33:59 +0700

    best

--- 6220502183_1.c
+++ 6220502183_1.c
@@ -2,7 +2,7 @@
 
 int main()
 {
-    int a,b,c,d,z,e,f,x;
+    int a,b,c,d,z,e,f,x,g,h;
 
     scanf("%d",&a);
     scanf("%d",&b);
@@ -11,6 +11,9 @@ int main()
     d=(b%10000)/100;
     e=a/10000;
     f=b/10000;
+    g=a%100;
+    h=b%100;
+    
     if (c==1||c==3||c==5||c==7||c==8||c==10||c==12)
     {
         z=31;
@@ -36,10 +39,15 @@ int main()
     {
         x=29;
     }
-   if (e<z&&f<x)
+   if (e<z&&f<x&&g==h)
    {
        printf("L");
    }
+    else if (e<z&&f<x&&g!=h)
+   {
+       printf("B");
+   }
+    
    else
    {
        printf("W");
