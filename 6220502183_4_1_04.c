35df5b32e2e4dc92064277f6a8b2aea623f17e13
saudrey <weerawutearth@gmail.com>
2021-03-04 15:11:12 +0700

    แ

--- 6220502183_4_1_04.c
+++ 6220502183_4_1_04.c
@@ -2,23 +2,5 @@
 
 int main()
 {
-    int i;
-    char a[200];
-
-    gets(a);
-
-
-    for ( i = 0; i < 200; i++)
-    {
-       if (a[i]==a[i+1])
-       {
-           continue;
-       }
-       else if (a[i]!=a[i+1])
-       {
-           printf("%c",a[i]);
-       }
-       
-    }
     
 }
