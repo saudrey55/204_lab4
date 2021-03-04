448280dab3261bef2c8f2c3fd4c6cb5fdf3e79d9
saudrey <weerawutearth@gmail.com>
2021-03-04 14:56:12 +0700

    Create 6220502183_4_3_04.c

---
+++ 6220502183_4_3_04.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+#include<string.h>
+
+int main()
+{
+    int i,z;
+    char a[200];
+
+    scanf("%s",a);
+    z=strlen(a);
+
+
+    for ( i = 0; i < z; i++)
+    {
+       if (a[i]==a[i+1])
+       {
+           continue;
+       }
+       else if (a[i]!=a[i+1])
+       {
+           printf("%c",a[i]);
+       }
+       
+    }
+    return 0;
+}
